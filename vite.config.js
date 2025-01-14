34297a8fac679c1b84b877bf962085f80b8d7976
praveem <praveen2272000.com>
2025-01-14 23:00:00 +0530

    New_commit

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: 'https://github.com/Praveenzoro/Tesla.git',
+  base: '/Tesla/',
   plugins: [react()],
 });
